c770ee279c904937b2a9469fc3800ffea1071e8e
imrkk <itsrkjii@gmail.com>
2022-06-04 15:49:14 +0530

    first commit

---
+++ src/Child.js
@@ -0,0 +1,14 @@
+
+function Child(p){
+
+    return(
+        <div>
+
+        <h1>Count : {p.data}</h1>
+
+        </div>
+    )
+
+}
+
+export default Child;
---
+++ src/Parent.js
@@ -0,0 +1,25 @@
+
+import React,{useState} from 'react'
+import Child from './Child'
+
+function Parent(){
+
+    const [count,setCount] = useState(0)
+    const num = 0;
+
+    return(
+        <div>
+
+        {
+            count>=0? <Child data={count} /> : <h1>Count : {num}</h1>
+        }
+        <button id='btn1' onClick={()=>setCount(count+1)}>Increment</button> <br/>
+        <button id='btn2' onClick={()=>setCount(count-1)}>Decrement</button>
+
+
+        </div>
+    )
+}
+
+export default Parent;
+
